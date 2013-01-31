dd996d31ee4cbfef366cd3f0b978a143e4228bf9
Lyle Troxell <lyle@troxell.com>
2013-01-31 12:32:21 -0800

    adding a file cleanup util

---
+++ Chickens/scripts/fixFiles.js
@@ -0,0 +1,30 @@
+﻿/* my chicken scratch to build my initial data import files */
+
+function createNameFilesFromCSV() {
+    var file = File(getFolder().path + "importData/names.csv");
+    var names = loadText(file);
+    var lines = names.split("\n");
+
+    var outFile = File(getFolder().path + "importData/henNames.text");
+    if (!outFile.exists) {
+        outFile.create();
+    }
+    var stream = TextStream(outFile, "write");
+    var results = 0;
+    var objRes = {};
+
+    lines.forEach(function(item) {
+        var arr = item.split(",");
+        var gender = arr[3].replace(/^[\s\"]+|[\s\"]+$/g, '')
+        var name = arr[1].replace(/^[\s\"]+|[\s\"]+$/g, '');
+
+        if (gender === "girl" && !objRes.hasOwnProperty(name)) {
+            objRes[name] = 1;
+            stream.write(name + "\n");
+            results++;
+        }
+    });
+    stream.close();
+    results; //just returns how many items are in the new file
+}
+
