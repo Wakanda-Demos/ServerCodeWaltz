f8dc903042bf51ee5937af6a729ae26f83542458
Lyle Troxell <lyle@troxell.com>
2013-03-05 08:30:37 -0800

    Better line breaks and generate two ways

--- Chickens/WebFolder/generateCoops.waPage/scripts/index.js
+++ Chickens/WebFolder/generateCoops.waPage/scripts/index.js
@@ -2,22 +2,22 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button1 = {};	// @button
 	var genDataButton = {};	// @button
+	var genDataWithWorkerButton = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	button1.click = function button1_click (event)// @startlock
+	genDataButton.click = function genDataButton_click (event)// @startlock
 	{// @endlock
 		$$("message").setValue("creating "+numberOfCoops+" coops")
 		$$("progressBar").startListening();
 		ds.Coop.callMethod( {
-			method: "generateCoopsWithWorker",
+			method: "generateCoops",
 			onSuccess: function(e) {
-				//$$("progressBar").stopListening();
-				//$$("progressBar").stopProgress();
-				//$$("message").setValue(e.result);
+				$$("progressBar").stopListening();
+				$$("progressBar").stopProgress();
+				$$("message").setValue(e.result);
 				console.log('success from server');
 			}, 
 			onError: function(e) {
@@ -28,7 +28,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 			numberOfCoops, "generateCoops");
 	};// @lock
 
-	genDataButton.click = function genDataButton_click (event)// @startlock
+	genDataWithWorkerButton.click = function genDataWithWorkerButton_click (event)// @startlock
 	{// @endlock
 		$$("message").setValue("creating "+numberOfCoops+" coops")
 		$$("progressBar").startListening();
@@ -49,7 +49,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button1", "click", button1.click, "WAF");
 	WAF.addListener("genDataButton", "click", genDataButton.click, "WAF");
+	WAF.addListener("genDataWithWorkerButton", "click", genDataWithWorkerButton.click, "WAF");
 // @endregion
 };// @endlock
