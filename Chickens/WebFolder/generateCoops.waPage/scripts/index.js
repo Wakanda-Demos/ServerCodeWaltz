4ad776817dd24f43d5e6029fb2fae7324e25d698
Lyle Troxell <lyle@troxell.com>
2013-03-05 08:06:33 -0800

    iAdd html with progress bars

---
+++ Chickens/WebFolder/generateCoops.waPage/scripts/index.js
@@ -0,0 +1,55 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+	var genDataButton = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		$$("message").setValue("creating "+numberOfCoops+" coops")
+		$$("progressBar").startListening();
+		ds.Coop.callMethod( {
+			method: "generateCoopsWithWorker",
+			onSuccess: function(e) {
+				//$$("progressBar").stopListening();
+				//$$("progressBar").stopProgress();
+				//$$("message").setValue(e.result);
+				console.log('success from server');
+			}, 
+			onError: function(e) {
+				$$("progressBar").stopListening();
+				$$("progressBar").stopProgress();
+				$$("message").setValue("An error happend:" + e.message);
+			}},
+			numberOfCoops, "generateCoops");
+	};// @lock
+
+	genDataButton.click = function genDataButton_click (event)// @startlock
+	{// @endlock
+		$$("message").setValue("creating "+numberOfCoops+" coops")
+		$$("progressBar").startListening();
+		ds.Coop.callMethod( {
+			method: "generateCoopsWithWorker",
+			onSuccess: function(e) {
+				//$$("progressBar").stopListening();
+				//$$("progressBar").stopProgress();
+				//$$("message").setValue(e.result);
+				console.log('success from server');
+			}, 
+			onError: function(e) {
+				$$("progressBar").stopListening();
+				$$("progressBar").stopProgress();
+				$$("message").setValue("An error happend:" + e.message);
+			}},
+			numberOfCoops, "generateCoops");
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("genDataButton", "click", genDataButton.click, "WAF");
+// @endregion
+};// @endlock
