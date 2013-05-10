50d4aa9f0a404fefa32750b5b50b97d4186dd02b
Lyle Troxell <lyle@troxell.com>
2013-05-10 16:20:01 -0700

    hack to note breed is changing

---
+++ Chickens/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,33 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var birdsEvent = {};	// @dataSource
+	var dataGrid4 = {};	// @dataGrid
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	birdsEvent.onCollectionChange = function birdsEvent_onCollectionChange (event)// @startlock
+	{// @endlock
+		$('#dataGrid4').removeClass('disable');
+
+	};// @lock
+
+	birdsEvent.onBeforeCurrentElementChange = function birdsEvent_onBeforeCurrentElementChange (event)// @startlock
+	{// @endlock
+		$('#dataGrid4').addClass('disable');
+	};// @lock
+
+
+	dataGrid4.onCellClick = function dataGrid4_onCellClick (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("birds", "onCollectionChange", birdsEvent.onCollectionChange, "WAF");
+	WAF.addListener("birds", "onBeforeCurrentElementChange", birdsEvent.onBeforeCurrentElementChange, "WAF");
+	WAF.addListener("dataGrid4", "onCellClick", dataGrid4.onCellClick, "WAF");
+// @endregion
+};// @endlock
